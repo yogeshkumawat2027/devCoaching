1130bc9966819845b3b7dfbf5aa8deeb8075c2d4
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2025-08-31 15:43:40 +0530

    refactor: migrate cloudinary configuration to ES module syntax

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,5 +1,7 @@
-const cloudinary = require('cloudinary').v2;
-require('dotenv').config();
+import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -7,4 +9,4 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-module.exports = cloudinary;
+export default cloudinary;
